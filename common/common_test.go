62b9d11039946cbc6f1f7991c059fc4a25217719
jakub <jakub.dalek@utoronto.ca>
2020-06-15 14:25:51 -0400

    testify added for reference proj

--- common/common_test.go
+++ common/common_test.go
@@ -1,12 +1,11 @@
 package common
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestAddNumbers(t *testing.T) {
-	answerShouldBe := 2
 	res := AddNumbers(1,1)
-
-	if res != answerShouldBe {
-		t.Errorf("add number failed should be %d but is %d", answerShouldBe, res)
-	}
+	assert.Equal(t, res, 2)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jakubd/referenceProject
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.6.0
+require (
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.2.2
+)
